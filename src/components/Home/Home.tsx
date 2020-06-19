5ee3502e676ef142830933f6664d032017c9b206
Michael Song <michael.song.hz@gmail.com>
2020-06-18 22:32:26 -0400

    Initial project bootstrapping

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+class Home extends React.Component {
+  render(): JSX.Element {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <p>hi</p>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Home;
