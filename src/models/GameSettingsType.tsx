34ad7ffc971be8ce2ce94adab02a0005b4a98d68
Michael Song <michael.song.hz@gmail.com>
2020-10-22 21:28:19 -0400

    Fixed square hero grid size

--- src/models/GameSettingsType.tsx
+++ src/models/GameSettingsType.tsx
@@ -11,7 +11,7 @@ type GridSizes = Record<GridSizeTypes, { rows: number; cols: number }>;
 export const gridSizes: GridSizes = {
   [GridSizeTypes.SMALL]: { rows: 5, cols: 6 },
   [GridSizeTypes.MEDIUM]: { rows: 7, cols: 7 },
-  [GridSizeTypes.LARGE_SQUARE]: { rows: 11, cols: 11 },
+  [GridSizeTypes.LARGE_SQUARE]: { rows: 9, cols: 11 },
   [GridSizeTypes.LARGE]: { rows: 6, cols: 16 },
   [GridSizeTypes.CUSTOM]: { rows: 0, cols: 0 },
 };
