acee66d241bce5756e07ee6bfc21d863c517a87c
Michael Song <michael.song.hz@gmail.com>
2020-10-27 23:52:17 -0400

    Added placeholders for when images are loading

--- src/components/GameShared/StatusBar/StatusBar.tsx
+++ src/components/GameShared/StatusBar/StatusBar.tsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
+import Placeholder_icon from "images/Placeholder_icon.png";
 import { useStoreState } from "reducer/store";
 import { heroList } from "utils/HeroList";
 import { prependCDN } from "utils/utilities";
 
 function GameStatusBar(): JSX.Element {
   const state = useStoreState();
+  const [loading, setLoading] = useState(true);
 
-  function renderHeroesToFind(): JSX.Element[] {
+  // Loads new icons if the current ones are changed
+  // Sets loading to true while icons are loading to display local placeholder
+  useEffect(() => {
+    const loadImage = (heroNumber: number) => {
+      return new Promise((resolve, reject) => {
+        const loadImg = new Image();
+        loadImg.src = prependCDN(heroList[heroNumber].url);
+        loadImg.onload = () => resolve(prependCDN(heroList[heroNumber].url));
+        loadImg.onerror = (err) => reject(err);
+      });
+    };
+
+    async function loadImages() {
+      setLoading(true);
+      await Promise.all(
+        Array.from(state.targetHeroes).map(
+          async (imageNumber) => await loadImage(imageNumber)
+        )
+      );
+      setLoading(false);
+    }
+
+    // Skips image loading if target icons will not be shown
+    if (state.gameSettings.showTargetIcons === false) {
+      setLoading(false);
+    } else {
+      loadImages();
+    }
+  }, [state.gameSettings.showTargetIcons, state.targetHeroes]);
+
+  function renderHeroesToFind(loadingIcons: boolean): JSX.Element[] {
     const heroesToFind: JSX.Element[] = [];
     state.targetHeroes.forEach((targetHero) => {
       heroesToFind.push(
@@ -17,7 +49,11 @@ function GameStatusBar(): JSX.Element {
           {state.gameSettings.showTargetIcons === true && (
             <img
               className="status-hero-icon"
-              src={prependCDN(heroList[targetHero].url)}
+              src={
+                loadingIcons
+                  ? Placeholder_icon
+                  : prependCDN(heroList[targetHero].url)
+              }
               alt={`${heroList[targetHero].name} icon`}
             ></img>
           )}
@@ -38,7 +74,18 @@ function GameStatusBar(): JSX.Element {
         )}
       </Col>
       <Col xs="12">
-        <Row className="justify-content-center">{renderHeroesToFind()}</Row>
+        <Row
+          className="justify-content-center"
+          style={{ display: loading ? "flex" : "none" }}
+        >
+          {renderHeroesToFind(true)}
+        </Row>
+        <Row
+          className="justify-content-center"
+          style={{ display: loading ? "none" : "flex" }}
+        >
+          {renderHeroesToFind(false)}
+        </Row>
       </Col>
     </Row>
   );
