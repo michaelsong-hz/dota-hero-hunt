027076eb99b20b20f5f6a0d225a9367759d3dfd3
Michael Song <michael.song.hz@gmail.com>
2020-11-02 00:09:33 -0500

    Removed background transition on page load

--- src/components/HelmetWrapper/HelmetWrapper.tsx
+++ src/components/HelmetWrapper/HelmetWrapper.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 
 import { useStoreState } from "reducer/store";
@@ -6,13 +6,32 @@ import { useStoreState } from "reducer/store";
 function HelmetWrapper(): JSX.Element {
   const state = useStoreState();
 
+  const [animateBgColorChange, setAnimateBgColorChange] = useState(false);
+
+  // Only add background change transition after the page loads
+  // Prevents white to dark transition on page load (if dark theme is on)
+  useEffect(() => {
+    setAnimateBgColorChange(true);
+  }, []);
+
+  function getClassName(): string {
+    let baseName = "";
+    if (state.appSettings.isDark) {
+      baseName += "helmet-body-dark";
+    } else {
+      baseName += "helmet-body-light";
+    }
+
+    if (animateBgColorChange) {
+      baseName += " helmet-body-change-transition";
+    }
+
+    return baseName;
+  }
+
   return (
     <Helmet>
-      <body
-        className={
-          state.appSettings.isDark ? "helmet-body-dark" : "helmet-body-light"
-        }
-      />
+      <body className={getClassName()} />
     </Helmet>
   );
 }
